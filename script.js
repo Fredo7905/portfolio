0448a05009512f819c5346ddd7ee05c1656dc9f7
Fredo7905 <wagenerfrederic@hotmail.com>
2025-03-27 15:27:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,14 +24,17 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelector(".nav-links");
+    const navLinks = document.querySelector(".nav-links");
 
-if (hamburger && navLinks) {
-    hamburger.addEventListener("click", () => {
-        navLinks.classList.toggle("active");
-    });
-}
+    if (hamburger && navLinks) {
+        hamburger.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
+        });
+    }
+});
+
 
 
 
